fe85631b7f506d5cf745cb9ec78b7cf59017cec0
Ayush Maheshwari <amaheshwari@simeiosolutions.com>
2020-10-04 18:19:25 +0530

    Updates

---
+++ precache-manifest.730529b706527b12bffefab4214bf330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f138e227d7ce3a7ef75069f403a4cb5",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "5be268f57c1c9b59dd88",
+    "url": "/monstors-rolodex/static/css/main.0864fb4a.chunk.css"
+  },
+  {
+    "revision": "2348cf9be702e2f66f64",
+    "url": "/monstors-rolodex/static/js/2.38a10f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors-rolodex/static/js/2.38a10f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be268f57c1c9b59dd88",
+    "url": "/monstors-rolodex/static/js/main.248c7c06.chunk.js"
+  },
+  {
+    "revision": "3166440b5c3dd3332041",
+    "url": "/monstors-rolodex/static/js/runtime-main.39cc9c93.js"
+  }
+]);
